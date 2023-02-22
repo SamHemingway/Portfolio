318c3f936d61462b9ba1d72b7283091e7f2ad692
Sam Hemingway <65196819+SamHemingway@users.noreply.github.com>
2023-02-22 13:19:04 -0500

    Turns out float was the answer

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -31,7 +31,11 @@ function Hero() {
 
   return (
     <section>
-      <div className={`${styles.heroWrapper}`}>
+      <div className={`${styles.heroWrapper} wrapper`}>
+        <Headshot
+          subject="sam"
+          altText="A stylised cariacuture of Sam smiling warmly."
+        />
         <motion.h1
           className={styles.heroHeader}
           variants={variants}
@@ -40,43 +44,23 @@ function Hero() {
         >
           hire a jnr frontend developer with a growth mindset
         </motion.h1>
-        <Headshot
-          subject="sam"
-          altText="A stylised cariacuture of Sam smiling warmly."
-        />
-        <article>
-          <div className={`${styles.leadContent}`}>
-            <h2>It's great to meet you.</h2>
-            <article className={styles.leadBody}>
-              <p>
-                I'm Sam: an experienced SaaS startup salesperson who realised
-                that the most satisfying part of his last role was building{" "}
-                <em>on-brand, accessible and fun</em> custom demo instances with
-                CSS to help my team try and land Fortune 100 companies.
-              </p>
-              <p>Why move to web dev?</p>
-              <ul>
-                <li>
-                  <em>I missed building things.</em>
-                </li>
-                <li>I've always been an undercover nerd.</li>
-              </ul>
-              <p>
-                I hope you can help me make a career of scratching these two
-                itches.
-              </p>
-            </article>
-          </div>
-        </article>
-        <div className={styles.test}></div>
       </div>
+      <article className={styles.leadWrapper}>
+        <div className={`wrapper`}>
+          <h2>It's great to meet you.</h2>
+          <p>
+            I'm Sam: an experienced SaaS startup salesperson who realised that
+            the most satisfying part of his last role was building{" "}
+            <em>on-brand, accessible and fun</em> custom demo instances with CSS
+            to help my team try and land Fortune 100 companies.
+          </p>
+        </div>
+      </article>
       <Pills
         content={skills}
         style={{
-          background: "var(--clr-secondary-300)",
-          color: "var(--clr-primary-700)",
-          marginBlockStart: "0",
-          paddingBlock: "1rem",
+          inlineSize: "clamp(343px, 90vw, 1080px)",
+          marginInline: "auto",
         }}
       />
     </section>
