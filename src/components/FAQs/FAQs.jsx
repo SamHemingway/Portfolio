c9ba89f906f3e4fafefdf00b5beb14d7f67bf3a4
Sam Hemingway <samphemingway@icloud.com>
2023-05-24 10:15:40 -0400

    Fixed formatting

--- src/components/FAQs/FAQs.jsx
+++ src/components/FAQs/FAQs.jsx
@@ -6,66 +6,87 @@ function FAQs() {
   const questions = [
     {
       title: "Where are you based?",
-      content:
-        "<p>Montréal, Canada, where I have permanent residency (I'm originally from the UK)!</p>",
+      content: (
+        <p>
+          Montréal, Canada, where I have permanent residency (I'm originally
+          from the UK)!
+        </p>
+      ),
       id: "where",
     },
     {
       title: "Do you speak French?",
-      content: `<p lang="fr">Oui, au niveau B2. Il faut dire que je n'ai jamais eu l'occasion de l'utiliser dans la vie professionnelle - tous les emplois que j'ai occupés jusqu'à présent l'ont été a les entreprises où l'anglais est la langue principale.</p>`,
+      content: (
+        <p lang="fr">
+          Oui, au niveau B2. Il faut dire que je n'ai jamais eu l'occasion de
+          l'utiliser dans la vie professionnelle - tous les emplois que j'ai
+          occupés jusqu'à présent l'ont été a les entreprises où l'anglais est
+          la langue principale.
+        </p>
+      ),
       id: "langue",
     },
     {
       title: "What makes you different?",
-      content: `<p>
+      content: (
+        <>
+          <p>
             Well it's certainly not a degree in Computer Science (I studied
             landscape architecture at school, in case you're curious!).
           </p>
-          <p>You're definitely interviewing more experienced candidates, with better technical skills than me.</p>
+          <p>
+            You're definitely interviewing more experienced candidates, with
+            better technical skills than me.
+          </p>
           <p>
             What differentiates me comes down to soft-skills. I've worked at
-            companies ranging from 
-            <a href="https://www.nationwide.co.uk">national banks</a>, to 
+            companies ranging from
+            <a href="https://www.nationwide.co.uk">national banks</a>, to
             <a href="https://www.poka.io">start-ups</a>, to being the 4th sales
-            hire at one of the 
+            hire at one of the
             <a href="https://www.hopin.com">
               fastest growing companies in history
-            </a> 
+            </a>
             who had a front row seat as it scaled from 100 to more than 1000
             employees in under a year.
           </p>
           <p>
-            When you hire me, you know that 
+            When you hire me, you know that
             <a href="#why">
               I already know how to be an effective and productive teammember
             </a>
             .
-          </p>`,
+          </p>
+        </>
+      ),
       id: "different",
     },
     {
       title: "12 months without work?",
-      content: `<p>A very valid question. Here's the short version:</p>
+      content: (
+        <>
+          <p>A very valid question. Here's the short version:</p>
           <ul>
+            <li>1 month to refresh and recharge post lay-off.</li>
             <li>
-              1 month to refresh and recharge post lay-off.
+              2 months interviewing for sales positions before realising I
+              didn't want to do it anymore.
             </li>
+            <li>4 months of debilitating long COVID symptoms — fun!</li>
             <li>
-              2 months interviewing for sales positions before
-              realising I didn't want to do it anymore.
+              5+ months of solid coding, with a focus on React and
+              accessibility.
             </li>
-            <li>
-              4 months of debilitating long COVID symptoms — fun!
-            </li>
-            <li>
-              5+ months of solid coding, with a focus on React and accessibility.
-            </li>
-          </ul>`,
+          </ul>
+        </>
+      ),
       id: "gap",
     },
     {
       title: "How did you learn to code?",
-      content: `<p>
+      content: (
+        <>
+          <p>
             Self-taught. I looked at boot camps, but outside of the networking
             opportunities, I wasn't convinced they were value for money.
           </p>
@@ -92,29 +113,42 @@ function FAQs() {
             <li>
               <a href="https://www.frontendmentor.io/profile/SamHemingway">
                 Frontend Mentor
-              </a> 
+              </a>
               to get experience building sites from Figma files.
             </li>
             <li>
-              <a href="https://www.codewars.com/users/SamHemingway">Codewars</a> 
+              <a href="https://www.codewars.com/users/SamHemingway">Codewars</a>
               to test my understanding of basic JS concepts.
             </li>
             <li>
-              Using official documentation to learn React libraries like 
-              <a href="https://www.framer.com/motion/">Framer Motion</a>, 
-              <a href="https://www.radix-ui.com/">Radix UI</a> & 
+              Using official documentation to learn React libraries like
+              <a href="https://www.framer.com/motion/">Framer Motion</a>,
+              <a href="https://www.radix-ui.com/">Radix UI</a> &
               <a href="https://headlessui.com/">Headless UI</a>.
             </li>
-          </ul>`,
+          </ul>
+        </>
+      ),
       id: "learn",
     },
     {
       title: "Career change & layoffs?",
-      content: `<p>
-            I know, I know. Crazy decision. <a href="https://hired.com/state-of-software-engineers/2023/">Interview interest for Junior engineers is flatlining</a>, and the safe option would've been to get another well-paying sales job.
+      content: (
+        <>
+          <p>
+            I know, I know. Crazy decision.{" "}
+            <a href="https://hired.com/state-of-software-engineers/2023/">
+              Interview interest for Junior engineers is flatlining
+            </a>
+            , and the safe option would've been to get another well-paying sales
+            job.
           </p>
-          <p>Blame my sales background: I relish the opportunity to win against the odds.</p>
-          `,
+          <p>
+            Blame my sales background: I relish the opportunity to win against
+            the odds.
+          </p>
+        </>
+      ),
       id: "change",
     },
   ];
